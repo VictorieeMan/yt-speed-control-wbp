b7cc5cfb700d32fbc12f7303298cd419ecd92e69
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-10-06 09:16:24 +0200

    added content.js

---
+++ mozilla/content.js
@@ -0,0 +1,23 @@
+function addSpeedControl() {
+    let controls = document.querySelector(".ytp-right-controls");
+    if (controls && !document.querySelector(".custom-speed-adjuster")) {
+        let btn = document.createElement("button");
+        btn.className = "ytp-button custom-speed-adjuster";
+        btn.innerText = "1.0x";
+        btn.onclick = function() {
+            let video = document.querySelector("video");
+            if (video) {
+                if (video.playbackRate === 1) {
+                    video.playbackRate = 1.25;
+                    btn.innerText = "1.25x";
+                } else {
+                    video.playbackRate = 1;
+                    btn.innerText = "1.0x";
+                }
+            }
+        };
+        controls.insertBefore(btn, controls.firstChild);
+    }
+}
+
+setInterval(addSpeedControl, 1000);
